92a0a57a21ac84806b524c716ef5f63df82f5d01
balchua@yahoo.com <balchua@yahoo.com>
2017-10-22 11:45:02 +0800

    rename project artifacts, added a demo project with standard springboot rest.

---
+++ src/main/java/org/bal/app/MainApplication.java
@@ -0,0 +1,23 @@
+package org.bal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+@RestController
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        LocalDateTime currentDate = LocalDateTime.now();
+        return "Hello World! -[" + currentDate + "]";
+    }
+}
