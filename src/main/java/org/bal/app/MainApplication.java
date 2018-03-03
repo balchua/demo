6cdcdbc6cdbcbdb958acaf16c178bcd00300aeb9
balchua@yahoo.com <balchua@yahoo.com>
2018-03-03 11:43:23 +0800

    added Springboot prometheus exporter. modified the port to use 9091 on proj demo.

--- src/main/java/org/bal/app/MainApplication.java
+++ src/main/java/org/bal/app/MainApplication.java
@@ -1,10 +1,13 @@
 package org.bal.app;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class MainApplication {
 
     public static void main(String[] args) {
