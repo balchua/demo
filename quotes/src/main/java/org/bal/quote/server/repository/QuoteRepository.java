be9634921d97c1584e62f943540cb0ca32d363e2
balchua <balchua@yahoo.com>
2019-07-20 12:10:24 +0800

    add resource requests and limits.  add Traced annotation to quotes.

--- quotes/src/main/java/org/bal/quote/server/repository/QuoteRepository.java
+++ quotes/src/main/java/org/bal/quote/server/repository/QuoteRepository.java
@@ -1,5 +1,6 @@
 package org.bal.quote.server.repository;
 
+import org.bal.quote.annotation.Traced;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +10,10 @@ import java.util.Optional;
 @Repository
 public interface QuoteRepository extends JpaRepository<QuoteEntity, Integer> {
 
+    @Traced(serviceName = "quote-find-by-id")
     Optional<QuoteEntity> findById(Integer id);
 
+    @Traced(serviceName = "quote-find-all")
     List<QuoteEntity> findAll();
 
 
---
+++ quotes/src/main/java/org/bal/quote/server/tracer/RepositoryAspectTracer.java
@@ -0,0 +1,48 @@
+package org.bal.quote.server.tracer;
+
+
+import brave.ScopedSpan;
+import brave.Tracer;
+import brave.Tracing;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.bal.quote.annotation.Traced;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class RepositoryAspectTracer {
+
+    private final Tracer tracer;
+
+    public RepositoryAspectTracer(@Autowired Tracing tracing) {
+        tracer = tracing.tracer();
+    }
+
+
+    @Around("@annotation(traced)")
+    public Object traceRepository(ProceedingJoinPoint pjp, Traced traced) throws Throwable {
+
+        log.debug("Tracing repository calls.");
+        ScopedSpan span = tracer.startScopedSpan(traced.serviceName());
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        span.tag("method", signature.getName());
+        try {
+            return pjp.proceed();
+        } catch (RuntimeException | Error e) {
+            span.error(e); // Unless you handle exceptions, you might not know the operation failed!
+            throw e;
+        } finally {
+            log.debug("After");
+            span.finish(); // always finish the span
+        }
+
+
+    }
+
+}
