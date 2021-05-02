0ef861b9a4fb9deb5775ac1a083480a6afc2bfb8
balchua <balchua@yahoo.com>
2021-05-02 15:16:49 +0800

    add more tests.

--- votes/src/test/java/org/bal/vote/server/VoteServerTest.java
+++ votes/src/test/java/org/bal/vote/server/VoteServerTest.java
@@ -3,6 +3,7 @@ package org.bal.vote.server;
 import com.google.protobuf.Empty;
 import io.grpc.ManagedChannel;
 import io.grpc.Server;
+import io.grpc.StatusRuntimeException;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
@@ -10,6 +11,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.bal.quote.proto.internal.Quote;
 import org.bal.vote.config.TestConfig;
+import org.bal.vote.proto.internal.Vote;
 import org.bal.vote.proto.internal.VoteManagementGrpc;
 import org.bal.vote.proto.internal.VoteRequest;
 import org.bal.vote.proto.internal.VotesList;
@@ -20,20 +22,16 @@ import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.annotation.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
@@ -115,6 +113,88 @@ class VoteServerTest {
         VotesList votes = blockingStub.getAllVotes(Empty.newBuilder().build());
         assertThat(votes.getVotesList().size()).isEqualTo(1);
         log.info("Current vote: " + votes.getVotes(0).getCount());
+
+    }
+
+    @Test
+    void shouldFailAVote() throws Exception {
+
+        VoteManagementGrpc.VoteManagementBlockingStub blockingStub =
+                VoteManagementGrpc.newBlockingStub(inProcessChannel);
+
+        VoteRequest vote = VoteRequest.newBuilder().setQuoteId(2).build();
+
+        Quote quote = Quote.newBuilder().setId(0).setName("Steve Rogers").setQuote("test me").build();
+
+        List<Quote> quotes = new ArrayList<>();
+        quotes.add(quote);
+
+        Long voteCasted = 1l;
+        doNothing().when(voteRepository).castVote(any(Integer.class));
+        when(voteRepository.getVote(any(Integer.class))).thenReturn(1);
+
+
+        when(quoteClient.allQuotes()).thenReturn(quotes);
+        when(quoteClient.getQuoteById(any(Integer.class))).thenReturn(quote);
+
+        assertThatThrownBy(() -> {
+            blockingStub.castVote(vote);
+        }).isInstanceOf(StatusRuntimeException.class);
+
+
+    }
+
+    @Test
+    void shouldGetAllVotes() throws Exception {
+
+        VoteManagementGrpc.VoteManagementBlockingStub blockingStub =
+                VoteManagementGrpc.newBlockingStub(inProcessChannel);
+
+        VoteRequest vote = VoteRequest.newBuilder().setQuoteId(0).build();
+
+        Quote quote = Quote.newBuilder().setId(0).setName("Steve Rogers").setQuote("test me").build();
+
+        List<Quote> quotes = new ArrayList<>();
+        quotes.add(quote);
+
+        Long voteCasted = 1l;
+        Vote theVote = Vote.newBuilder().setCount(1).build();
+        List<Vote> votes = new ArrayList<>();
+        votes.add(theVote);
+        doNothing().when(voteRepository).castVote(any(Integer.class));
+        when(voteRepository.getAllVotes(any())).thenReturn(votes);
+        when(quoteClient.allQuotes()).thenReturn(quotes);
+        when(quoteClient.getQuoteById(any(Integer.class))).thenReturn(quote);
+
+        VotesList votesList= blockingStub.getAllVotes(Empty.newBuilder().build());
+        assertThat(votesList.getVotesList().size()).isEqualTo(1);
+
+    }
+
+    @Test
+    void shouldGetAllVotesUsingMultiGet() throws Exception {
+
+        VoteManagementGrpc.VoteManagementBlockingStub blockingStub =
+                VoteManagementGrpc.newBlockingStub(inProcessChannel);
+
+        VoteRequest vote = VoteRequest.newBuilder().setQuoteId(0).build();
+
+        Quote quote = Quote.newBuilder().setId(0).setName("Steve Rogers").setQuote("test me").build();
+
+        List<Quote> quotes = new ArrayList<>();
+        quotes.add(quote);
+
+        Long voteCasted = 1l;
+        Vote theVote = Vote.newBuilder().setCount(1).build();
+        List<Vote> votes = new ArrayList<>();
+        votes.add(theVote);
+        doNothing().when(voteRepository).castVote(any(Integer.class));
+        when(voteRepository.getAllVotes(any())).thenReturn(votes);
+        when(quoteClient.allQuotes()).thenReturn(quotes);
+        when(quoteClient.getQuoteById(any(Integer.class))).thenReturn(quote);
+
+        VotesList votesList= blockingStub.getAllVotesWithMultiget(Empty.newBuilder().build());
+        assertThat(votesList.getVotesList().size()).isEqualTo(1);
 
     }
 
